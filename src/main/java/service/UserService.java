c0e7ce01a3128f322f7c7c39b95cf664adddd8f0
jeong-inho <derosatam76@gmail.com>
2021-09-01 16:54:44 +0900

    chore: 불필요한 메소드 삭제

--- src/main/java/service/UserService.java
+++ src/main/java/service/UserService.java
@@ -10,13 +10,7 @@ import static db.DataBase.matchUserByIdAndPassword;
 public class UserService {
 
     public void save(Map<String, String> userInfo) {
-        saveUser(User.of(userInfo));
-    }
-
-    private void saveUser(User user) {
-        if (user != null) {
-            addUser(user);
-        }
+        addUser(User.of(userInfo));
     }
 
     public boolean login(Map<String, String> userInfo) {
